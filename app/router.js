45cac40fb2d828e0ca0c2e3c890ab5bc3f289853
chenyunfei <438376555@qq.com>
2018-03-26 17:29:55 +0800

    '不使用fs方式'

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@
 module.exports = app => {
   const { router, controller } = app;
 
-  router.get('/', controller.home.index);
+  // router.get('/', controller.home.index);
 
   router.post('/oauth', controller.wxsdk.oauth);
 
