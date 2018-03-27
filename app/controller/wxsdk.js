a1eaaa2462ac62b5c85686641f3269a40630d9d0
chenyunfei <438376555@qq.com>
2018-03-27 13:03:30 +0800

    绑定至东农助手公众号

--- app/controller/wxsdk.js
+++ app/controller/wxsdk.js
@@ -5,8 +5,9 @@ const axios = require('axios');
 const path = require('path');
 const fs = require('fs');
 const config = {
-  appId: 'wx9fd6bbc89436a5ee',
-  appSecret: '338e4bea61cc75fe5f69ad9a1416e893',
+  appId: 'wx3ff5c48ba9ac6552',
+  appSecret: '4e7735e37c9c493a5ead5cb07c6cd6fd',
+  // appSecret: '338e4bea61cc75fe5f69ad9a1416e893',
   jsUrl: 'https://neau-lib.xiaonei.io/feed',
 };
 // 缓存access_token
